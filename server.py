2e7675e74d8f1e1f668aefecce6309303c54c3c7
Jaskirat Billing <Billingjas22@gmail.com>
2019-01-29 18:47:27 -0800

    commit

--- server.py
+++ server.py
@@ -1,42 +1,80 @@
-from flask import Flask, jsonify
-from flask import render_template
 import requests
 
-app = Flask(__name__)
+from flask import Flask 
+from flask import render_template
+
+app =Flask(__name__)
+
 
 @app.route('/')
-def home_page():
+def HackerNewsCode():
 	response = requests.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
 
-	top_five_ids = response.json()[:10]
+# Get top 5 ids
+	top_five_ids = response.json()[:5]
 
-	print('Top five ids:', top_five_ids)
-
-	comments = {}
+# Fetch top 5 stories
 
 	stories = []
 
-	for id in top_five_ids:
-	    curr_story_api_url = 'https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty'.format(id)
-	    curr_story = requests.get(curr_story_api_url)
-	    stories.append(curr_story.json())
+	for top_id in top_five_ids:
+	curr_story_api_url = 'https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty'.format(id)
+	curr_story = requests.get(curr_story_api_url)
+	stories.append(curr_story.json())
 
+	listOfStories = []
 
 	for story in stories:
-		if 'kids' in story:
-		    comment_ids = story['kids'][:100]
-		    title = story['title']
-		    comments[title] = []
+	title = story['title'] 
+	comments = [] 
+	dictionary = {}  
+
+	for kid in story['kids'][:5]:
+	   
+	   kid_url = requests.get('https://hacker-news.firebaseio.com/v0/item/' +str(kid)+'.json?print=pretty')
+	   comment = kid_url.json()['text'] 
+	   comments.append(comment) 
+	   dictionary[title] = comments 
+	   listOfStories.append(dictionary) 
 
 
-	for comment_id in comment_ids:
-	    curr_comment_api_url = 'https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty'.format(id)
-	    curr_comment = requests.get(curr_comment_api_url)
-	    comments[title].append(curr_comment.json())
+class HackerNews:
+	with open('goodwords.txt', 'r') as open_pos:
+		positive_vocab=open_pos.readlines()
 
-	return jsonify(comments)
+	with open('badwords.txt', 'r') as open_neg:
+		negative_vocab=open_neg.readlines()
+	
+	def __init__(self, positive_vocab, negative_vocab):
+		self.positive_vocab = positive_vocab
+		self.negative_vocab = negative_vocab
 
+	
+	def analyze_news(hacker):
+		words = hacker.split()
 
+		num_positive_vocab = 0 
+		num_negative_vocab = 0 
+		for word in words:
+		 
+			if word in positive_vocab:
+				num_positive_vocab += 1
+			elif word in negative_vocab:
+				num_negative_vocab += 1 
 
-if __name__ == '__main__':
-	app.run()
+		if num_positive_vocab > num_negative_vocab:
+			return "positive"
+
+		elif num_positive_vocab < num_negative_vocab:
+			return "negative"
+		else: 
+			return "inconclusive" 
+
+		for sentence in listOfStories:
+			hacker = sentence.split(",")[3].strip()
+
+			analyze_news(hacker)
+	
+	hacker = input("What do you want to analyze?")
+	analyze_news(hacker)
+	print(analyze_news(hacker))
